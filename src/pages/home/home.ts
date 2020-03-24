7ff26013ecd5164735605dfc0cedb0b2d065ac03
jocimarlopes <lopes.jocimar@gmail.com>
2020-03-24 02:21:29 -0300

    Atualizaçoes - Horarios de Onibus

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertController: AlertController) {
 
+  }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      title: 'Sobre',
+      message: 'Desenvolvido por <a href="https://jocimarlopes.com.br">Jocimar Lopes</a>',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,28 +1,98 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['time'];
+  public dias: Array<any>;
+  public domingo: Array<any>;
+  public sabado: Array<any>;
+  public util: Array<any>;
+  constructor() {
     
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
+  segmentChanged(ev: any) {
+    console.log('Segment changed', ev);
+  }
+  
+  ngOnInit() {
+
+    this.domingo= [
+      {hora: '07:00'},
+      {hora: '08:00'},
+      {hora: '09:00'},
+      {hora: '10:00'},
+      {hora: '11:00'},
+      {hora: '12:00'},
+      {hora: '09:30'},
+      {hora: '10:00'},
+      {hora: '11:00'},
+      {hora: '11:35'},
+      {hora: '12:00'},
+      {hora: '14:00'},
+      {hora: '15:00'},
+      {hora: '16:00'},
+      {hora: '17:00'},
+      {hora: '18:00'},
+      {hora: '19:00'},
+      {hora: '20:00'},
+    ],
+    this.sabado= [
+      {hora: '06:20'}, 
+      {hora: '06:50'}, 
+      {hora: '07:30'}, 
+      {hora: '07:50'}, 
+      {hora: '08:30'}, 
+      {hora: '09:00'}, 
+      {hora: '10:00'}, 
+      {hora: '11:00'}, 
+      {hora: '11:35'}, 
+      {hora: '12:00'}, 
+      {hora: '12:30'}, 
+      {hora: '12:45'}, 
+      {hora: '13:30'}, 
+      {hora: '14:00'}, 
+      {hora: '15:00'}, 
+      {hora: '16:00'}, 
+      {hora: '17:00'}, 
+      {hora: '18:00'}, 
+      {hora: '19:00'},
+      {hora: '20:00'},
+    ],
+    this.util= [
+      {hora: '05:20'}, 
+      {hora: '06:20'}, 
+      {hora: '07:30'}, 
+      {hora: '07:50'}, 
+      {hora: '08:30'}, 
+      {hora: '09:00'}, 
+      {hora: '09:30'}, 
+      {hora: '10:00'}, 
+      {hora: '11:00'}, 
+      {hora: '11:35'}, 
+      {hora: '12:00'}, 
+      {hora: '12:30'}, 
+      {hora: '12:45'}, 
+      {hora: '13:30'}, 
+      {hora: '14:00'}, 
+      {hora: '14:30'}, 
+      {hora: '15:00'}, 
+      {hora: '15:30'}, 
+      {hora: '16:00'}, 
+      {hora: '16:30'}, 
+      {hora: '17:00'},
+      {hora: '17:35'},
+      {hora: '18:00'},
+      {hora: '18:35'},
+      {hora: '19:00'},
+      {hora: '19:40'},
+      {hora: '20:40'},
+    ],
+
+    this.dias= [
+      {uteis: 'Dias úteis', sab: 'Sábado', dom: 'Domingo'},
+    ]
   }
 }
